c5440552a5d68dd85b88faa87bef58a5bb583065
Mario_void <drakenmario@gmail.com>
2025-06-07 03:29:46 -0600

    Merge master and add Prettier

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  js.configs.recommended,
+  ...ts.configs["flat/recommended"],
+  {
+    files: ["**/*.{ts,tsx}"],
+    ignores: ["dist"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
